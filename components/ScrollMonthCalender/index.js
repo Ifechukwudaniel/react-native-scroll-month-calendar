8d3067517fec6fdd147fb9b31e76bfdf686ba38e
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-10 12:27:18 +0100

    Added scroll month calender components

---
+++ components/ScrollMonthCalender/index.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import style from './style'
+
+type Props = {};
+export default class NumberView extends Component<Props> {
+  render() {
+    return (
+  
+    );
+  }
+}
+ 
+NumberView.defaultProps = {
+   initialValue:0,
+   enableDefaultStyles:true,
+   themeColor:false,
+   maxLimit:false
+}
